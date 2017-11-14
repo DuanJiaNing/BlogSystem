1946c2e801e4bea658dc2de8d100b4c13ab2ead4
DuanJiaNing <aimeimeits@gmail.com>
2017-11-14 20:08:02 +0800

    clear ignore

---
+++ src/test/java/com/duan/blogos/BaseTest.java
@@ -0,0 +1,15 @@
+package com.duan.blogos;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created on 2017/11/14.
+ *
+ * @author DuanJiaNing
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
+public abstract class BaseTest {
+}
